d66d8ecb7ba266a7e67126ce6473504e4efc5ab0
Zac Brown <zacbrown@users.noreply.github.com>
2018-10-20 11:40:36 -0700

    Basic ETW monitor that looks for .NET core DLL mscoree.dll.
    Signed-off-by: Zac Brown <zacbrown@users.noreply.github.com>

---
+++ EtwDotNetLoadMonitor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.O365.Security.ETW;
+
+namespace EtwDotNetLoadMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trace = new UserTrace();
+            var processProvider = new Provider("Microsoft-Windows-Kernel-Process");
+            processProvider.All = 0x40; // Enable the WINEVENT_KEYWORD_IMAGE flag.
+            var filter = new EventFilter(Filter.EventIdIs(5));
+
+            filter.OnEvent += (record) =>
+            {
+                var dllName = record.GetUnicodeString("ImageName", "<UNKNOWN>");
+                if (dllName.ToLower().EndsWith("mscoree.dll"))
+                {
+                    var pid = record.GetUInt32("ProcessID", 0);
+                    var processName = string.Empty;
+
+                    try { processName = System.Diagnostics.Process.GetProcessById((int)pid).ProcessName; }
+                    catch (Exception) { }
+                    Console.WriteLine($"{processName} (PID: {pid}) loaded .NET runtime ({dllName})");
+                }
+            };
+
+            processProvider.AddFilter(filter);
+            trace.Enable(processProvider);
+
+            Console.CancelKeyPress += (sender, eventArg) =>
+            {
+                if (trace != null)
+                {
+                    trace.Stop();
+                }
+            };
+
+            trace.Start();
+        }
+    }
+}
